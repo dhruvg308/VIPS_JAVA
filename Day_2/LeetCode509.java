84459c2991c3198ce5793a0878ab82a9ca717769
Dhruv Gupta <dhruvg308@gmail.com>
2026-07-09 19:43:21 +0530

    Day-2 completed

---
+++ Day_2/LeetCode509.java
@@ -0,0 +1,74 @@
+/*
+===============================================================================
+509. Fibonacci Number (Easy)
+===============================================================================
+
+The Fibonacci numbers, commonly denoted F(n), form a sequence where each
+number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0
+F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1
+
+Given n, calculate F(n).
+
+Example 1:
+Input:  n = 2
+Output: 1
+
+Explanation:
+F(2) = F(1) + F(0) = 1 + 0 = 1
+
+Example 2:
+Input:  n = 3
+Output: 2
+
+Explanation:
+F(3) = F(2) + F(1) = 1 + 1 = 2
+
+Example 3:
+Input:  n = 4
+Output: 3
+
+Explanation:
+F(4) = F(3) + F(2) = 2 + 1 = 3
+
+Constraints:
+0 <= n <= 30
+
+===============================================================================
+================= Given Code: ================================================
+
+class Solution {
+    public int fib(int n) {
+
+    }
+}
+
+*/
+
+package Day_2;
+
+class Solution {
+    public int fib(int n) {
+        int a = 0;
+        int b = 1;
+
+        int i=0;
+        while(i<n){
+            int c = a+b;
+            a = b;
+            b = c;
+            i++;
+        }
+        return a;
+    }
+}
+
+public class LeetCode509 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.fib(6));
+    }
+    
+}
