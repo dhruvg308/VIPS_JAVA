00f11f635bea5f15b776ddc3f1e4cc3c72bd6dc4
Dhruv Gupta <dhruvg308@gmail.com>
2026-07-15 12:12:47 +0530

    Day-3 Q5

---
+++ Day_3/Q5.java
@@ -0,0 +1,25 @@
+// // Q5.convert decimal to binary number
+
+// package Day_3;
+
+// import java.util.Scanner;
+
+// public class Q5 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the Decimal number: ");
+//         int n = sc.nextInt();
+
+//         System.out.print("Its binary representation is: ");
+
+//         int original =n;
+
+//         int bin = 0;
+//         while(n!=0) {
+//             if(n%2==)
+            
+//         }   
+//     sc.close();
+//     }
+    
+// }
