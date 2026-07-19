bde295d85c7504933e72fa973175479f19cb0f50
Dhruv Gupta <dhruvg308@gmail.com>
2026-07-19 11:13:26 +0530

    Day-3 Q12

---
+++ Day_3/Q12.java
@@ -0,0 +1,36 @@
+/*
+                       1
+                      121
+                     12321
+                    1234321
+
+*/
+package Day_3;
+
+import java.util.Scanner;
+
+public class Q12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height of the pattern: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+
+            for(int j=i-1; j>0; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+
+        sc.close();
+    }
+}
