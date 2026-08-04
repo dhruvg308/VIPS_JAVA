b180f837d327984190543d37125427ca7e46c9c3
Dhruv Gupta <dhruvg308@gmail.com>
2026-08-04 16:54:33 +0530

    Day-7 LC-744

---
+++ Day_7/LeetCode744.java
@@ -0,0 +1,31 @@
+package Day_7;
+
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start =0;
+        int end = letters.length-1;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(letters[mid]>target){
+                end = mid-1;
+            }
+            else{
+                start = mid +1;
+            }
+        }
+
+     return letters[start % letters.length];
+    }
+}
+
+public class LeetCode744 {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        char arr[] = {'c','f','j'};
+    }
+}
